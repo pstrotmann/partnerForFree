c16b7df6a53b53c60795970f0a3782461690326b
pstrotmann <peter@strotmann.org>
2015-09-24 17:04:32 +0200

    Rücksetzen AppUser Notiz korigiert

--- Partner/grails-app/conf/org/strotmann/benutzer/SecurityFilters.groovy
+++ Partner/grails-app/conf/org/strotmann/benutzer/SecurityFilters.groovy
@@ -9,24 +9,23 @@ class SecurityFilters {
 		    before = {
 				if (controllerName == 'partner')
 					return
-				Benutzer u = appUser(controllerName)
-				if (u) 
-					session.user = appUser()
+				Benutzer u = appUser()
+				if (u) {
+					session.user = u
+					Notiz.executeUpdate("update Notiz n set n.referenz='0' where n.notiztext = 'appUser'")
+				}
 				if (!session.user ) 
 					redirect(controller: "benutzer", action: "login")
             }
         }
     }
 	
-	Benutzer appUser(String ctrlName){
+	Benutzer appUser(){
 		Benutzer b = null
 		def Notiz n = Notiz.find("from Notiz as n where n.notiztext = 'appUser'")
 		if (n) {  
-			if (Long.parseLong(n.referenz) > 0) {
+			if (Long.parseLong(n.referenz) > 0) 
 				b = Benutzer.get(Long.parseLong(n.referenz))
-				if (ctrlName == 'assets') 
-					Notiz.executeUpdate("update Notiz n set n.referenz='0' where n.notiztext = 'appUser'")
-			}
 		}
 		b
 	}
